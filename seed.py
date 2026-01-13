8f73f6915a41ef82c2601c5804e4e0f222f4a404
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2026-01-13 20:10:34 +0300

    Commit database session

--- seed.py
+++ seed.py
@@ -19,7 +19,7 @@ with app.app_context():
     g2 = Guest(name="Sandra Bernhard", occupation="Comedian")
     g3 = Guest(name="Tracey Ullman", occupation="television actress")
     db.session.add_all([g1, g2, g3])
-    
+    db.session.commit()
 
     print("Creating appearances...")
     a1 = Appearance(rating=4, episode_id=ep1.id, guest_id=g1.id)
